d3be1a65b8db0bb641bdac2ff9a10fe96f98dc24
frknshnklksz <furkankulaksiz68@gmail.com>
2020-11-03 22:00:58 +0300

    Initial Commit

---
+++ src/main/java/com/adakapital/adakapitalbackendcase/AdakapitalbackendcaseApplication.java
@@ -0,0 +1,13 @@
+package com.adakapital.adakapitalbackendcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdakapitalbackendcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdakapitalbackendcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adakapital/adakapitalbackendcase/AdakapitalbackendcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adakapital.adakapitalbackendcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdakapitalbackendcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
